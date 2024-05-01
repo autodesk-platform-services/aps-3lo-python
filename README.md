6b75b20359910724e76c7d726294e7db88d88b76
JoaoMartins-Forge <joao.martins@autodesk.com>
2024-04-30 21:15:56 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # aps-3lo-python
-Sample to obtain 3LO using python on a minimal server
+
+Minimal sample to obtain 3LO using python
+
+## Setup
+
+Replace the configuration variables with your own:
+
+```
+app.config['CLIENT_ID'] = 'YOUR CLIENT ID'
+app.config['CLIENT_SECRET'] = 'YOUR CLIENT SECRET'
+app.config['CALLBACK_URL'] = 'http://localhost:8080/api/auth/callback'
+app.config['SCOPES'] = 'data:read viewables:read'
+```
+
+install flask and requests in your environment:
+`pip install flask`
+`pip install requests`
+
+Run the app:
+`python app.py`
+
+Then go to `localhost:8080` in your browser and login with your Autodesk account:
+
+![Thumbnail](./thumbnail.gif)
+
+## License
+
+This sample is licensed under the terms of the [MIT License](http://opensource.org/licenses/MIT). Please see the [LICENSE](LICENSE) file for full details.
+
+## Written by
+
+Joao Martins [in/jpornelas](https://linkedin.com/in/jpornelas), [Developer Advocate](http://aps.autodesk.com)
---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, redirect
+import requests
+
+app = Flask(__name__)
+
+# Configuration variables
+app.config['CLIENT_ID'] = ''
+app.config['CLIENT_SECRET'] = ''
+app.config['CALLBACK_URL'] = 'http://localhost:8080/api/auth/callback'
+app.config['SCOPES'] = 'data:read viewables:read'
+
+@app.route('/')
+def hello():
+    return redirect(f"https://developer.api.autodesk.com/authentication/v2/authorize?response_type=code&client_id={app.config['CLIENT_ID']}&redirect_uri={app.config['CALLBACK_URL']}&scope={app.config['SCOPES']}")
+
+@app.route('/api/auth/callback', methods=['POST','GET'])
+def callback():
+  # Get credential code
+  code = request.args.get('code')
+  tokenUrl = "https://developer.api.autodesk.com/authentication/v2/token"
+  payload = f"grant_type=authorization_code&code={code}&client_id={app.config['CLIENT_ID']}&client_secret={app.config['CLIENT_SECRET']}&redirect_uri={app.config['CALLBACK_URL']}"
+  headers = {
+      "Content-Type": "application/x-www-form-urlencoded"
+  }
+  resp = requests.request("POST", tokenUrl, data=payload, headers=headers)
+  respJson = resp.json()
+  # Return success response
+  return f"{respJson}", 200
+
+if __name__ == '__main__':
+  app.run(debug=True, port=8080)
