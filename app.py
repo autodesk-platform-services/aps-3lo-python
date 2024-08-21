f3a0ea098d88b1cc6ae1a146228dc7091b934766
Joao Martins <75683555+JoaoMartins-callmeJohn@users.noreply.github.com>
2024-08-21 11:39:43 -0300

    Update app.py
    Add refresh logic

--- app.py
+++ app.py
@@ -19,12 +19,26 @@ def callback():
   payload = f"grant_type=authorization_code&code={code}&client_id={os.getenv('CLIENT_ID')}&client_secret={os.getenv('CLIENT_SECRET')}&redirect_uri={os.getenv('CALLBACK_URL')}"
   tokenUrl = "https://developer.api.autodesk.com/authentication/v2/token"
   headers = {
-      "Content-Type": "application/x-www-form-urlencoded"
+    "Content-Type": "application/x-www-form-urlencoded"
   }
   resp = requests.request("POST", tokenUrl, data=payload, headers=headers)
   respJson = resp.json()
+  refresh_token = respJson["refresh_token"]
   # Return success response
-  return f"{respJson}", 200
+  return f"{respJson}<br><br><a href=\"/api/auth/refresh?refresh_token={refresh_token}\">Refresh Token</a>", 200
+
+@app.route('/api/auth/refresh', methods=['POST','GET'])
+def refresh():
+  refreshToken = request.args.get('refresh_token')
+  payload = f"grant_type=refresh_token&refresh_token={refreshToken}&client_id={os.getenv('CLIENT_ID')}&client_secret={os.getenv('CLIENT_SECRET')}"
+  tokenUrl = "https://developer.api.autodesk.com/authentication/v2/token"
+  headers = {
+    "Content-Type": "application/x-www-form-urlencoded"
+  }
+  resp = requests.request("POST", tokenUrl, data=payload, headers=headers)
+  respJson = resp.json()
+  refresh_token = respJson["refresh_token"]
+  return f"{respJson}<br><br><a href=\"/api/auth/refresh?refresh_token={refresh_token}\">Refresh Token</a>", 200
 
 if __name__ == '__main__':
   app.run(debug=True, port=8080)
